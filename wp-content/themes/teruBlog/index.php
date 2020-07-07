826c175d0b4ee5af80ffe2f7c3b5a1b4739749be
teruyainomata0831 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-07 17:57:14 +0900

    title変更

--- wp-content/themes/teruBlog/index.php
+++ wp-content/themes/teruBlog/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>teruBlog</title>
+  <title>terublog</title>
   <link rel="shortcut icon" href="wp-content/themes/twentynineteen/favicon.ico">
   <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,700&display=swap" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
