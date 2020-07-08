73a2dce0a9f6d7a27be9c8bd7d3bd2b955c7e43d
teruyainomata0831 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-08 16:04:56 +0900

    footer ウィジェット追加

--- wp-content/themes/teruBlog/footer.php
+++ wp-content/themes/teruBlog/footer.php
@@ -1,7 +1,9 @@
 <aside>
-  <ul><?php dynamic_sidebar('フッターウィジェット１'); ?></ul>
-  <ul><?php dynamic_sidebar('フッターウィジェット２'); ?></ul>
-  <ul><?php dynamic_sidebar('フッターウィジェット３'); ?></ul>
+  <div class="footerBox">
+    <ul><?php dynamic_sidebar('フッターウィジェット１'); ?></ul>
+    <ul><?php dynamic_sidebar('フッターウィジェット２'); ?></ul>
+    <ul><?php dynamic_sidebar('フッターウィジェット３'); ?></ul>
+  </div>
 </aside>
 <footer>
   <p> Copyright - teruya inomata, 2020 All Rights Reserved.</p>
