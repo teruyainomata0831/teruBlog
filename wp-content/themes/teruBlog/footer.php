f08051ccdf2ce9126f76d90a68cc497fa9179963
teruyainomata0831 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-08 15:46:12 +0900

    footer ウィジェット追加

--- wp-content/themes/teruBlog/footer.php
+++ wp-content/themes/teruBlog/footer.php
@@ -1,3 +1,8 @@
+<aside>
+  <ul><?php dynamic_sidebar('フッターウィジェット１'); ?></ul>
+  <ul><?php dynamic_sidebar('フッターウィジェット２'); ?></ul>
+  <ul><?php dynamic_sidebar('フッターウィジェット３'); ?></ul>
+</aside>
 <footer>
   <p> Copyright - teruya inomata, 2020 All Rights Reserved.</p>
   </div>
--- wp-content/themes/teruBlog/functions.php
+++ wp-content/themes/teruBlog/functions.php
@@ -1,19 +1,23 @@
 <?php
+
 /**
  * Register our sidebars and widgetized areas.
  *
  */
-function arphabet_widgets_init() {
+function arphabet_widgets_init()
+{
 
-	register_sidebar( array(
+	register_sidebar(array(
 		'name' => 'register_sideber',
-    'id' => 'sidebar',
-    'description' => 'サイドバーウィジェット',
+		'id' => 'sidebar',
+		'description' => 'サイドバーウィジェット',
 		'before_widget' => '<div>',
 		'after_widget' => '</div>',
 		'before_title' => '<h2 class="rounded">',
 		'after_title' => '</h2>',
-	) );
+	));
+	register_sidebar(array('name' => 'フッターウィジェット１'));
+	register_sidebar(array('name' => 'フッターウィジェット２'));
+	register_sidebar(array('name' => 'フッターウィジェット３'));
 }
-add_action( 'widgets_init', 'arphabet_widgets_init' );
-?>
+add_action('widgets_init', 'arphabet_widgets_init');
