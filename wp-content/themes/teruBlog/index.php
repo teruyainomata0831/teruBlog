3228540c815880e0bbecde1ba56456aa3704e8c0
teruyainomata <inomata@tiltrotor.co.jp>
2020-07-12 16:21:36 +0900

    同じカテゴリーの投稿にリンクが飛ぶように指定

--- wp-content/themes/teruBlog/index.php
+++ wp-content/themes/teruBlog/index.php
@@ -25,11 +25,11 @@
     </div>
     <div class="stepback">
       <?php if (get_previous_post()) : ?>
-        <?php previous_post_link('%link', '&laquo; 前の記事へ'); ?>
+        <?php previous_post_link('%link', '&laquo; 前の記事へ', true); ?>
       <?php endif; ?>
 
       <?php if (get_next_post()) : ?>
-        <?php next_post_link('%link', '次の記事へ &raquo;'); ?>
+        <?php next_post_link('%link', '次の記事へ &raquo;', true); ?>
       <?php endif; ?>
     </div>
   </div>
