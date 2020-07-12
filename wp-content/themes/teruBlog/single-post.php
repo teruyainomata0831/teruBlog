1650526654cbb43936a070c2b6a572a7d1fba5eb
teruyainomata <inomata@tiltrotor.co.jp>
2020-07-12 15:07:17 +0900

    修正

---
+++ wp-content/themes/teruBlog/single-post.php
@@ -0,0 +1,45 @@
+<!-- postの内容が表示される -->
+<?php get_header(); ?>
+<section id="content">
+  <div id="content-wrap" class="container" style="width: 70%">
+    <div id="main" class="col-md-9">
+      <div class="time">
+        <?php the_time('Y/m/d'); ?>
+      </div>
+      <?php
+      if (have_posts()) :
+        while (have_posts()) : the_post();
+      ?>
+          <h1><?php the_title(); ?></h1>
+          <section>
+            <?php the_content(); ?>
+          </section>
+        <?php endwhile; ?>
+      <?php else : ?>
+        <div class="error">
+          <p>お探しの記事は見つかりませんでした。</p>
+        </div>
+      <?php
+      endif;
+      ?>
+    </div>
+    <div class="stepback">
+      <?php if (get_previous_post()) : ?>
+        <?php previous_post_link('%link', '&laquo; 前の記事へ'); ?>
+      <?php endif; ?>
+
+      <?php if (get_next_post()) : ?>
+        <?php next_post_link('%link', '次の記事へ &raquo;'); ?>
+      <?php endif; ?>
+    </div>
+  </div>
+  <?php get_sidebar(); ?>
+</section>
+<aside>
+  <div class="footerBox">
+    <ul><?php dynamic_sidebar('フッターウィジェット１'); ?></ul>
+    <ul><?php dynamic_sidebar('フッターウィジェット２'); ?></ul>
+    <ul><?php dynamic_sidebar('フッターウィジェット３'); ?></ul>
+  </div>
+</aside>
+<?php get_footer(); ?>
--- wp-content/themes/teruBlog/single.php
+++ wp-content/themes/teruBlog/single.php
@@ -1,9 +1,11 @@
 <?php
 $post = $wp_query->post;
-if (in_category('travel')) {
-  include(TEMPLATEPATH . '/single-travel.php');
+if (in_category('post')) {
+  include(TEMPLATEPATH . '/single-post.php');
 } elseif (in_category('skill')) {
   include(TEMPLATEPATH . '/single-skill.php');
+} elseif (in_category('travel')) {
+  include(TEMPLATEPATH . '/single-travel.php');
 } else {
   include(TEMPLATEPATH . '/index.php');
 }
