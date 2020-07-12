fd85699f2c017cc295f7ffb901f30674f29f325b
teruyainomata <inomata@tiltrotor.co.jp>
2020-07-12 14:55:23 +0900

    修正

--- wp-content/themes/teruBlog/single.php
+++ wp-content/themes/teruBlog/single.php
@@ -1,36 +1,9 @@
-<!-- postの内容が表示される -->
-<?php get_header(); ?>
-<section id="content">
-  <div id="content-wrap" class="container" style="width: 70%">
-    <div id="main" class="col-md-9">
-      <div class="time">
-        <?php the_time('Y/m/d'); ?>
-      </div>
-      <?php
-      if (have_posts()) :
-        while (have_posts()) : the_post();
-      ?>
-          <h1><?php the_title(); ?></h1>
-          <section>
-            <?php the_content(); ?>
-          </section>
-        <?php endwhile; ?>
-      <?php else : ?>
-        <div class="error">
-          <p>お探しの記事は見つかりませんでした。</p>
-        </div>
-      <?php
-      endif;
-      ?>
-    </div>
-  </div>
-  <?php get_sidebar(); ?>
-</section>
-<aside>
-  <div class="footerBox">
-    <ul><?php dynamic_sidebar('フッターウィジェット１'); ?></ul>
-    <ul><?php dynamic_sidebar('フッターウィジェット２'); ?></ul>
-    <ul><?php dynamic_sidebar('フッターウィジェット３'); ?></ul>
-  </div>
-</aside>
-<?php get_footer(); ?>
+<?php
+$post = $wp_query->post;
+if (in_category('travel')) {
+  include(TEMPLATEPATH . '/single-travel.php');
+} elseif (in_category('skill')) {
+  include(TEMPLATEPATH . '/single-skill.php');
+} else {
+  include(TEMPLATEPATH . '/index.php');
+}
