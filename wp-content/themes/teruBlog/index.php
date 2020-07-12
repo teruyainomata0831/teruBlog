1e7e9f325c0899905f32f37a5fa39f43b8bf9a97
teruyainomata <inomata@tiltrotor.co.jp>
2020-07-12 13:45:07 +0900

    次へ、前へbtn作成

--- wp-content/themes/teruBlog/index.php
+++ wp-content/themes/teruBlog/index.php
@@ -23,6 +23,15 @@
       endif;
       ?>
     </div>
+    <div class="stepback">
+      <?php if (get_previous_post()) : ?>
+        <?php previous_post_link('%link', '&laquo; 前の記事へ'); ?>
+      <?php endif; ?>
+
+      <?php if (get_next_post()) : ?>
+        <?php next_post_link('%link', '次の記事へ &raquo;'); ?>
+      <?php endif; ?>
+    </div>
   </div>
   <?php get_sidebar(); ?>
 </section>
