a6d05e390492c4fce12367363622e538485aede7
teruyainomata <inomata@tiltrotor.co.jp>
2020-07-15 12:42:19 +0900

    メール送信機能 追加

--- wp-content/themes/teruBlog/functions.php
+++ wp-content/themes/teruBlog/functions.php
@@ -62,3 +62,20 @@ function register_my_menus()
 	));
 }
 add_action('after_setup_theme', 'register_my_menus');
+
+remove_action('add_option_new_admin_email', 'update_option_new_admin_email');
+remove_action('update_option_new_admin_email', 'update_option_new_admin_email');
+
+/**
+ * Disable the confirmation notices when an administrator
+ * changes their email address.
+ *
+ * @see http://codex.wordpress.com/Function_Reference/update_option_new_admin_email
+ */
+function wpdocs_update_option_new_admin_email($old_value, $value)
+{
+
+	update_option('admin_email', $value);
+}
+add_action('add_option_new_admin_email', 'wpdocs_update_option_new_admin_email', 10, 2);
+add_action('update_option_new_admin_email', 'wpdocs_update_option_new_admin_email', 10, 2);
