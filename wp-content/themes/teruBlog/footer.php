e987a72abfd6784fa5072a6a9073df15a11c59a1
猪股輝哉 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-16 21:41:16 +0900

    ptengine追加

--- wp-content/themes/teruBlog/footer.php
+++ wp-content/themes/teruBlog/footer.php
@@ -4,6 +4,20 @@
   </div>
 </footer>
 <?php wp_footer(); ?>
+<script type="text/javascript">
+  window._pt_lt = new Date().getTime();
+  window._pt_sp_2 = [];
+  _pt_sp_2.push("setAccount,41729f4c");
+  var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+  (function() {
+    var atag = document.createElement("script");
+    atag.type = "text/javascript";
+    atag.async = true;
+    atag.src = _protocol + "js.ptengine.jp/41729f4c.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(atag, s);
+  })();
+</script>
 </body>
 
 </html>
