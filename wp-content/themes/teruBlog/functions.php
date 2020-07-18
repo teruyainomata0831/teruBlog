b2ee688f526b1051e4a1eaad75621cdc9b3c791f
猪股輝哉 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-18 20:57:48 +0900

    コミット

--- wp-content/themes/teruBlog/functions.php
+++ wp-content/themes/teruBlog/functions.php
@@ -63,42 +63,41 @@ function register_my_menus()
 }
 add_action('after_setup_theme', 'register_my_menus');
 
-function breadcrumb(){
-	global $post;
-	$str ='';
-	if(!is_home()&&!is_admin()){
-			$str.= '<div id="breadcrumb" class="cf"><div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;">';
-			$str.= '<a href="'. home_url() .'" itemprop="url"><span itemprop="title">Home</span></a> &gt;&#160;</div>';
-			if(is_category()) {
-					$cat = get_queried_object();
-					if($cat -> parent != 0){
-							$ancestors = array_reverse(get_ancestors( $cat -> cat_ID, 'category' ));
-							foreach($ancestors as $ancestor){
-$str.='<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;"><a href="'. get_category_link($ancestor) .'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor) .'</span></a> &gt;&#160;</div>';
-							}
-					}
-$str.='<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;"><a href="'. get_category_link($cat -> term_id). '" itemprop="url"><span itemprop="title">'. $cat-> cat_name . '</span></a> &gt;&#160;</div>';
-			} elseif(is_page()){
-					if($post -> post_parent != 0 ){
-							$ancestors = array_reverse(get_post_ancestors( $post->ID ));
-							foreach($ancestors as $ancestor){
-									$str.='<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;"><a href="'. get_permalink($ancestor).'" itemprop="url"><span itemprop="title">'. get_the_title($ancestor) .'</span></a> &gt;&#160;</div>';
-							}
-					}
-			} elseif(is_single()){
-					$categories = get_the_category($post->ID);
-					$cat = $categories[0];
-					if($cat -> parent != 0){
-							$ancestors = array_reverse(get_ancestors( $cat -> cat_ID, 'category' ));
-							foreach($ancestors as $ancestor){
-									$str.='<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;"><a href="'. get_category_link($ancestor).'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor). '</span></a>→</div>';
-							}
-					}
-					$str.='<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb" style="display:table-cell;"><a href="'. get_category_link($cat -> term_id). '" itemprop="url"><span itemprop="title">'. $cat-> cat_name . '</span></a> &gt;&#160;</div>';
-			} else{
-					$str.='<div>'. wp_title('', false) .'</div>';
-			}
-			$str.='</div>';
+function mytheme_breadcrumb() {
+	$sep = '>';
+	echo '<li><a href="'.get_bloginfo('url').'" >Home</a></li>';
+	echo $sep;
+
+	$cats = '';
+	$cat_id = '';
+	if ( is_single() ) {
+		$cats = get_the_category();
+		if( isset($cats[0]->term_id) ) $cat_id = $cats[0]->term_id;
 	}
-	echo $str;
+	else if ( is_category() ) {
+		$cats = get_queried_object();
+		$cat_id = $cats->parent;
+	}
+	$cat_list = array();
+	while ($cat_id != 0){
+		$cat = get_category( $cat_id );
+		$cat_link = get_category_link( $cat_id );
+		array_unshift( $cat_list, '<a href="'.$cat_link.'">'.$cat->name.'</a>' );
+		$cat_id = $cat->parent;
+	}
+	foreach($cat_list as $value){
+		echo '<li>'.$value.'</li>';
+		echo $sep;
+	}
+
+	if ( is_singular() ) {
+		if ( is_attachment() ) {
+			previous_post_link( '<li>%link</li>' );
+			echo $sep;
+		}
+		the_title( '<li>', '</li>' );
+	}
+	else if( is_archive() ) the_archive_title( '<li>', '</li>' );
+	else if( is_search() ) echo '<li>検索 : '.get_search_query().'</li>';
+	else if( is_404() ) echo '<li>ページが見つかりません</li>';
 }
