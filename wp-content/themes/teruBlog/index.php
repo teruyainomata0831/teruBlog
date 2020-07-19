45c893c04c975e6f36abe2d717f84397ea5afa47
猪股輝哉 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-19 15:44:54 +0900

    コミット

--- wp-content/themes/teruBlog/index.php
+++ wp-content/themes/teruBlog/index.php
@@ -23,6 +23,10 @@
       endif;
       ?>
     </div>
+    <div id="related-entries">
+      <h3>関連記事</h3>
+      <?php include( TEMPLATEPATH . '/related-entries.php' ); ?>
+    </div>
     <div class="stepback">
       <?php if (get_previous_post()) : ?>
         <?php previous_post_link('%link', '&laquo; 前の記事へ', true); ?>
