a17740327a86ba7b602ee8be93c1282a5872e38f
猪股輝哉 <teruyainomata@inomataakiranarinoMacBook-Pro.local>
2020-07-30 16:49:01 +0900

    クイックタグ追加

--- wp-content/themes/teruBlog/functions.php
+++ wp-content/themes/teruBlog/functions.php
@@ -131,51 +131,10 @@ function add_my_quicktag() {
 	QTags.addButton('waku-blue-dashed', 'waku-blue-dashed', '<ul class="waku-blue-dashed">', '</ul>');
 	QTags.addButton('waku-gray-border', 'waku-gray-border', '<div class="waku-gray-border">', '</div>');
 	QTags.addButton('important', 'important', '<span class="important">', '</span>');
+	QTags.addButton('check', 'check', '<div class="sgb-heading__inner sgb-heading--type1 with-icon" style="background-color:transparent;/* border-color:transparent; */font-size:1.2em;"><i class="fa fa-check" style="color:#5ba9f7"></i>', '</div>');
 	QTags.addButton('ymarker', 'ymarker', '<span class="ymarker">', '</span>');
 	QTags.addButton('rmarker', 'rmarker', '<span class="rmarker">', '</span>');
 	</script>
 	<?php
 }
 add_action('admin_print_footer_scripts', 'add_my_quicktag');
-
-function mytheme_breadcrumb() {
-	//HOME>と表示
-	$sep = '>';
-	echo '<li><a href="'.get_bloginfo('url').'" >Home</a></li>';
-	echo $sep;
- 
-	//投稿記事ページとカテゴリーページでの、カテゴリーの階層を表示
-	$cats = '';
-	$cat_id = '';
-	if ( is_single() ) {
-		$cats = get_the_category();
-		if( isset($cats[0]->term_id) ) $cat_id = $cats[0]->term_id;
-	}
-	else if ( is_category() ) {
-		$cats = get_queried_object();
-		$cat_id = $cats->parent;
-	}
-	$cat_list = array();
-	while ($cat_id != 0){
-		$cat = get_category( $cat_id );
-		$cat_link = get_category_link( $cat_id );
-		array_unshift( $cat_list, '<a href="'.$cat_link.'">'.$cat->name.'</a>' );
-		$cat_id = $cat->parent;
-	}
-	foreach($cat_list as $value){
-		echo '<li>'.$value.'</li>';
-		echo $sep;
-	}
- 
-	//現在のページ名を表示
-	if ( is_singular() ) {
-		if ( is_attachment() ) {
-			previous_post_link( '<li>%link</li>' );
-			echo $sep;
-		}
-		the_title( '<li>', '</li>' );
-	}
-	else if( is_archive() ) the_archive_title( '<li>', '</li>' );
-	else if( is_search() ) echo '<li>検索 : '.get_search_query().'</li>';
-	else if( is_404() ) echo '<li>ページが見つかりません</li>';
-}
